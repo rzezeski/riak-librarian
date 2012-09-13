ad0d63f72ee88ea52e30f4f426ecce03f37baba9
Ryan Zezeski <rzezeski@gmail.com>
2012-09-13 16:34:33 -0400

    Wrap in commit element

--- git-log-xml.sh
+++ git-log-xml.sh
@@ -5,4 +5,4 @@
 # Usage:
 #   ./git-log-xml.sh > log.xml
 
-git log --pretty=format:'<field name="commit">%h</field>%n<field name="author">%an</field>%n<field name="date">%ai</field>%n<field name="subject_body">%s%n%b<field>%n'
+git log --pretty=format:'<commit>%n<field name="hash">%h</field>%n<field name="author">%an</field>%n<field name="date">%ai</field>%n<field name="subject_body">%s%n%b<field>%n</commit>'
