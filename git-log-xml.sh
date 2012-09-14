90ed11031d72f13bfb074ce0a203c18f2403d1e2
Ryan Zezeski <rzezeski@gmail.com>
2012-09-14 18:22:54 -0400

    Add ability to specify git dir

--- git-log-xml.sh
+++ git-log-xml.sh
@@ -2,7 +2,38 @@
 
 # Format git log as XML
 #
-# Usage:
-#   ./git-log-xml.sh > log.xml
+#> Usage:
+#>   ./git-log-xml.sh [ -d GIT_DIR ] > log.xml
 
-git log --pretty=format:'<commit>%n<field name="hash">%h</field>%n<field name="author">%an</field>%n<field name="date">%ai</field>%n<field name="subject_body">%s%n%b<field>%n</commit>'
+GIT_OPTS=""
+
+usage() {
+    echo
+    grep '^#>' $0 | tr -d '#>'
+}
+
+error() {
+    msg=$1
+
+    echo $msg
+    usage
+    exit 1
+}
+
+while test $# -gt 0
+do
+    case $1 in
+        -d)
+            GIT_OPTS="$GIT_OPTS --git-dir=$2/.git --work-tree=$2"
+            ;;
+        -*)
+            error "Unrecognized option: $1"
+            ;;
+        *)
+            break
+            ;;
+    esac
+    shift
+done
+
+git $GIT_OPTS log --pretty=format:'<commit>%n<field name="hash">%h</field>%n<field name="author">%an</field>%n<field name="date">%ai</field>%n<field name="subject_body">%s%n%b<field>%n</commit>'
