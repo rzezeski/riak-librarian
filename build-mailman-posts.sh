e5cbee5a1941286792ffad494b67a662d7d5b4cc
Ryan Zezeski <rzezeski@gmail.com>
2012-09-18 11:55:01 -0400

    Add script to build posts from a mailman archive

---
+++ build-mailman-posts.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Build indexable posts from a mailman archive.
+#
+# Usage:
+#     ./build-mailman-posts.sh URL DIR
+#
+# Example:
+#     ./build-mailman-posts.sh http://lists.basho.com/pipermail/riak-users_lists.basho.com/ riak-users
+
+URL=$1
+shift
+DIR=$1
+shift
+
+tmpdir=$(mktemp -d $DIR-XXXXX)
+archives=$(curl $URL | sed -nE '/.*href="(.*\.txt\.gz)".*/ {s//\1/; p; }')
+
+for name in $archives; do
+    archive_url=$URL/$name
+    file=$tmpdir/$name
+    txt=${file%.gz}
+    xml="${txt%.txt}.xml"
+
+    wget -P $tmpdir $archive_url
+    gunzip $file
+    ./mailman-to-xml.sed $txt > $xml
+    rm $txt
+done
+
+mv $tmpdir $DIR
+
