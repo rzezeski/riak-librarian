a73b07be5d7f74afd4d592c2b8ed21f94b263dce
Ryan Zezeski <rzezeski@gmail.com>
2012-09-26 01:52:18 -0400

    WIP: change build script

--- build-mailman-posts.sh
+++ build-mailman-posts.sh
@@ -24,7 +24,8 @@ for name in $archives; do
 
     wget -P $tmpdir $archive_url
     gunzip $file
-    ./mailman-to-xml.sed $txt > $xml
+    gawk -f mailman.awk $txt > $xml
+    # ./mailman-to-xml.sed $txt > $xml
     rm $txt
 done
 
