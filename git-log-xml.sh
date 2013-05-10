52e3f35e54f1d5cb79d1843f8e0cf97af59f35a3
Ryan Zezeski <rzezeski@gmail.com>
2013-05-09 21:02:57 -0400

    Change format of XML chunk a bit

--- git-log-xml.sh
+++ git-log-xml.sh
@@ -3,9 +3,10 @@
 # Format git log as XML
 #
 #> Usage:
-#>   ./git-log-xml.sh [ -d GIT_DIR ] > log.xml
+#>   ./git-log-xml.sh [ -d GIT_DIR ] > log.txt
 
 GIT_OPTS=""
+DIR=$PWD
 
 usage() {
     echo
@@ -24,7 +25,9 @@ while test $# -gt 0
 do
     case $1 in
         -d)
-            GIT_OPTS="$GIT_OPTS --git-dir=$2/.git --work-tree=$2"
+            shift
+            GIT_OPTS="$GIT_OPTS --git-dir=$1/.git --work-tree=$1"
+            DIR=$1
             ;;
         -*)
             error "Unrecognized option: $1"
@@ -36,4 +39,8 @@ do
     shift
 done
 
-git $GIT_OPTS log --pretty=format:'<commit>%n<hash_s>%h</hash_s>%n<author_ws>%an</author_ws>%n<date_ig>%ai</date_ig>%n<subject_body_t><![CDATA[%s%n%b]]></subject_body_t>%n</commit>'
+REPO_NAME=$(basename $DIR)
+
+# TODO: convert time to UTC, right now just pretending local is UTC
+git $GIT_OPTS log --pretty=format:"<commit>%n<repo_s>$REPO_NAME</repo_s>%n<hash_s>%H</hash_s>%n<author_s>%an</author_s>%n<author_dt>%at</author_dt>%n<committer_dt>%ct</committer_dt>%n<subject_body_tsd><![CDATA[%s%n%b]]></subject_body_tsd>%n</commit>" \
+    | gawk 'match($0, /(<.*_dt>)(.*)(<.*_dt>)/, x) { printf("%s%s%s\n", x[1], strftime("%Y-%m-%dT%H:%M:%SZ", x[2]), x[3]); next; } { print $0 }'
